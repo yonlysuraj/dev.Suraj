b941b6958e7500d117b8fa64ddcf644ae4a25f31
yonlysuraj <mail4suraj2002@gmail.com>
2025-03-19 16:31:30 +0530

    Changed Hero Section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,11 +7,15 @@ const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={pfp} alt="" />
-      <h1><span>Hey! I'm Suraj Mallick,</span><br/>a Full Stack Developer</h1>
-      <p>Im a Full stack Developer in Odisha, I have skills that might hep you to grow your company</p>
-      <div className="hero-action">
-        <div className="hero-connect"><AnchorLink className='acnchor-link' offset={50} href='#contact'>Connect with me</AnchorLink></div>
-        <div className="hero-resume">My resume</div>
+      <div className="hero-content">
+        <h1><span>Hey! I'm Suraj Mallick,</span><br/>a Full Stack Developer</h1>
+        <p>Im a Full stack Developer in Odisha, I have skills that might hep you to grow your company</p>
+        <div className="hero-action">
+          <div className="hero-connect">
+            <AnchorLink className='acnchor-link' offset={50} href='#contact'>Connect with me</AnchorLink>
+          </div>
+          <div className="hero-resume">My resume</div>
+        </div>
       </div>
     </div>
   )
