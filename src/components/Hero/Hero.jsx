2978da4c5ab1bab462a76a69850d3013f4a2ca18
yonlysuraj <mail4suraj2002@gmail.com>
2025-03-19 16:35:44 +0530

    Type writer effect added to Hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './Hero.css'
 import pfp from '../../assets/pfp.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
+import { TypeAnimation } from 'react-type-animation'
 
 const Hero = () => {
   return (
@@ -9,7 +10,20 @@ const Hero = () => {
       <img src={pfp} alt="" />
       <div className="hero-content">
         <h1><span>Hey! I'm Suraj Mallick,</span><br/>a Full Stack Developer</h1>
-        <p>Im a Full stack Developer in Odisha, I have skills that might hep you to grow your company</p>
+        <TypeAnimation
+          sequence={[
+            'Im a Full stack Developer in Odisha, I have skills that might help you to grow your company',
+            1000,
+            'I build responsive web applications',
+            1000,
+            'I create modern user interfaces',
+            1000,
+          ]}
+          wrapper="p"
+          speed={50}
+          repeat={Infinity}
+          style={{ fontSize: '22px', color: '#888', lineHeight: '1.6' }}
+        />
         <div className="hero-action">
           <div className="hero-connect">
             <AnchorLink className='acnchor-link' offset={50} href='#contact'>Connect with me</AnchorLink>
