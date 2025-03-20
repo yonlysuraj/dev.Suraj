8fc7801099ec71772fa6413926c012c084513a66
yonlysuraj <mail4suraj2002@gmail.com>
2025-03-20 08:15:24 +0530

    Made icon-color effect in Services component

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,49 +1,45 @@
-import React from 'react'
-import './Services.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import ServicesData from '../../assets/services_data'
-import { 
-  FaReact, 
-  FaCode, 
-  FaAws, 
-  FaTerminal, 
-  FaDatabase, 
-  FaGithub 
-} from 'react-icons/fa'
+import React from "react";
+import "./Services.css";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import ServicesData from "../../assets/services_data";
+import { FaReact, FaCode, FaAws, FaTerminal, FaDatabase, FaGithub } from "react-icons/fa";
+
+const icons = [
+  { component: FaReact, color: "#61DBFB" },
+  { component: FaCode, color: "#ff6f00" },
+  { component: FaAws, color: "#FF9900" },
+  { component: FaTerminal, color: "#00ff00" },
+  { component: FaDatabase, color: "#00758f" },
+  { component: FaGithub, color: "#ffff" }
+];
 
 const Services = () => {
-  const getIcon = (index) => {
-    switch(index) {
-      case 0: return <FaReact size={48} className="service-icon" />
-      case 1: return <FaCode size={48} className="service-icon" />
-      case 2: return <FaAws size={48} className="service-icon" />
-      case 3: return <FaTerminal size={48} className="service-icon" />
-      case 4: return <FaDatabase size={48} className="service-icon" />
-      case 5: return <FaGithub size={48} className="service-icon" />
-      default: return null
-    }
-  }
-
   return (
-    <div id='services' className='services'>
+    <div id="services" className="services">
       <div className="services-title">
         <h1>My Services</h1>
-        <img src={theme_pattern} alt="No img" />
+        <img src={theme_pattern} alt="Theme Pattern" />
       </div>
       <div className="services-container">
-        {ServicesData.map((service, index) => (
-          <div key={index} className="services-format">
-            <div className="service-header">
-              <span className="service-number">{service.s_no}</span>
-              {getIcon(index)}
+        {ServicesData.map((service, index) => {
+          const IconComponent = icons[index]?.component || FaCode;
+          return (
+            <div key={index} className="services-format">
+              <div className="service-header">
+                <span className="service-number">{service.s_no}</span>
+                <IconComponent
+                  size={48}
+                  className="service-icon"
+                />
+              </div>
+              <h2>{service.s_name}</h2>
+              <p>{service.s_desc}</p>
             </div>
-            <h2>{service.s_name}</h2>
-            <p>{service.s_desc}</p>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Services
+export default Services;
