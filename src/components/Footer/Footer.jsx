9c8e925a75053e4fc21d5d4365521f5ca5502fe3
yonlysuraj <mail4suraj2002@gmail.com>
2025-03-20 17:41:24 +0530

    Made some styling changes in contact and footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,30 +4,32 @@ import user_icon from '../../assets/user_icon.svg'
 import './Footer.css'
 const Footer = () => {
   return (
-    <div className='footer'>
-      <div className="footer-top">
-        <div className="footer-top-left">
-            <h1>dev.Suraj</h1>
-            <p>I'm a Developer here to learn</p>
+    <footer className='footer'>
+      <div className="footer-content">
+        <div className="footer-top">
+          <div className="footer-top-left">
+              <h1>dev.Suraj</h1>
+              <p>I'm a Developer here to learn</p>
+          </div>
+          <div className="footer-top-right">
+              <div className="footer-email-input">
+                  <img src={user_icon} alt="" />
+                  <input type="email" placeholder='Enter your Email:' />
+              </div>
+              <div className="footer-subscribe">Subscribe</div>
+          </div>
         </div>
-        <div className="footer-top-right">
-            <div className="footer-email-input">
-                <img src={user_icon} alt="" />
-                <input type="email" placeholder='Enter your Email:' />
-            </div>
-            <div className="footer-subscribe">Subscribe</div>
+        <hr />
+        <div className="footer-bottom">
+          <p className="footer-bottom-left">@Suraj Mallick.All rights reserved</p>
+          <div className="footer-bottom-right">
+              <p>Terms of services</p>
+              <p>Privacy Policy</p>
+              <p>Connect with me</p>
+          </div>
         </div>
       </div>
-      <hr />
-      <div className="footer-bottom">
-        <p className="footer-bottom-left">@Suraj Mallick.All rights reserved</p>
-        <div className="footer-bottom-right">
-            <p>Terms of services</p>
-            <p>Privacy Policy</p>
-            <p>Connect with me</p>
-        </div>
-      </div>
-    </div>
+    </footer>
   )
 }
 
