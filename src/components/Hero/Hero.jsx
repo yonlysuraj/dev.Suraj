a85ac9a29c4dbef0e04718d75d66b8f5ddfa9bfa
yonlysuraj <mail4suraj2002@gmail.com>
2025-03-20 23:20:00 +0530

    Added social media icons on hero component and made the mobile view look good

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,20 +3,25 @@ import './Hero.css'
 import pfp from '../../assets/pfp.jpg'
 import AnchorLink from 'react-anchor-link-smooth-scroll'
 import { TypeAnimation } from 'react-type-animation'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faInstagram, faDiscord, faXTwitter } from '@fortawesome/free-brands-svg-icons'
 
 const Hero = () => {
   return (
     <div id='home' className='hero'>
       <img src={pfp} alt="" />
       <div className="hero-content">
-        <h1><span>Hey! I'm Suraj Mallick,</span><br/>a Full Stack Developer</h1>
+        <h1>
+          <span>Hey! I'm Suraj Mallick,</span><br/>
+          a Cloud Engineer
+        </h1>
         <TypeAnimation
           sequence={[
-            'Im a Full stack Developer in Odisha, I have skills that might help you to grow your company',
+            'Im a Cloud Engineer in Odisha, I have skills that might help you to grow your company',
             1000,
-            'I build responsive web applications',
+            'I build cloud infrastructure',
             1000,
-            'I create modern user interfaces',
+            'I create scalable solutions',
             1000,
           ]}
           wrapper="p"
@@ -29,6 +34,23 @@ const Hero = () => {
             <AnchorLink className='acnchor-link' offset={50} href='#contact'>Connect with me</AnchorLink>
           </div>
           <div className="hero-resume">My resume</div>
+        </div>
+        <div className="hero-social">
+          <a href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faXTwitter} />
+          </a>
+          <a href="https://linkedin.com/in/yourusername" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://github.com/yonlysuraj" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://discord.com/users/yourid" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faDiscord} />
+          </a>
         </div>
       </div>
     </div>
